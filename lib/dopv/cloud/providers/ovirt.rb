10dcaa9b617495522113d65664c529ea4e2f7540
Pavol Dilung <pavol.dilung@swisscom.com>
2015-01-25 12:05:14 +0100

    Implement begin/rescue block in Dopv::Ovirt::Node, improve Dopv::Ovir::Node#gwt_ovirt_ca_cert

--- lib/dopv/cloud/providers/ovirt.rb
+++ lib/dopv/cloud/providers/ovirt.rb
@@ -35,7 +35,6 @@ module Dopv
       }
 
       class Node < BaseNode
-
         def initialize(config)
           @config = config
           cloud_init = { :hostname => @config[:nodename] }
@@ -45,41 +44,48 @@ module Dopv
             cloud_init[:netmask] = @config[:nets][0][:netmask]
             cloud_init[:gateway] = @config[:nets][0][:gateway]
           end
-
-          @compute_client = Fog::Compute.new(
-            :provider           => @config[:provider],
-            :ovirt_username     => @config[:provider_username],
-            :ovirt_password     => @config[:provider_password],
-            :ovirt_url          => @config[:provider_endpoint],
-            :ovirt_ca_cert_file => get_ovirt_ca_cert
-          )
-          vm = @compute_client.servers.create(
-            :name     => @config[:nodename],
-            :template => get_template_id,
-            :cores    => FLAVOR[@config[:flavor].to_sym][:cores],
-            :memory   => FLAVOR[@config[:flavor].to_sym][:memory],
-            :storage  => FLAVOR[@config[:flavor].to_sym][:storage]
-          )
-          vm.wait_for { !locked? }
-          vm.service.vm_start_with_cloudinit(:id => vm.id, :user_data => cloud_init)
-          vm.reload
+          begin
+            # Create new compute client instance
+            @compute_client = Fog::Compute.new(
+              :provider           => @config[:provider],
+              :ovirt_username     => @config[:provider_username],
+              :ovirt_password     => @config[:provider_password],
+              :ovirt_url          => @config[:provider_endpoint],
+              :ovirt_ca_cert_file => get_ovirt_ca_cert
+            )
+            # Create new virtual machine instance
+            vm = @compute_client.servers.create(
+              :name     => @config[:nodename],
+              :template => get_template_id,
+              :cores    => FLAVOR[@config[:flavor].to_sym][:cores],
+              :memory   => FLAVOR[@config[:flavor].to_sym][:memory],
+              :storage  => FLAVOR[@config[:flavor].to_sym][:storage]
+            )
+            vm.wait_for { !locked? }
+            vm.service.vm_start_with_cloudinit(:id => vm.id, :user_data => cloud_init)
+            vm.reload
+          rescue Exception => e 
+            raise Errors::ProviderError, "Error while creating '#{@config[:nodename]}': #{e}"
+          end
         end
 
         private
 
         def get_ovirt_ca_cert
           uri = URI.parse(@config[:provider_endpoint])
-          local_ca_file = "/tmp/#{uri.hostname}_#{uri.port}_ca.crt"
+          local_ca_file  = "/tmp/#{uri.hostname}_#{uri.port}_ca.crt"
           remote_ca_file = "#{uri.scheme}://#{uri.host}:#{uri.port}/ca.crt"
+          local_ca = remote_ca = nil
           unless File.exists?(local_ca_file)
             begin
-              open(remote_ca_file) do |r|
-                f = File.open(local_ca_file, 'w')
-                f.write(r.read)
-                f.close
-              end
+              remote_ca = open(remote_ca_file, 'r')
+              local_ca  = open(local_ca_file, 'w')
+              local_ca.write(remote_ca.read)
             rescue
-              raise Dopv::Errors::ProviderError, "Cannot download CA certificate from #{uri.host}"
+              raise Errors::ProviderError, "Cannot download CA certificate from #{uri.host}"
+            ensure
+              remote_ca.close if remote_ca
+              local_ca.close if local_ca
             end
           end
           local_ca_file
@@ -88,7 +94,7 @@ module Dopv
         def get_template_id
           id = nil
           @compute_client.list_templates.each { |t| id = t[:raw].id if t[:raw].name == @config[:image] }
-          raise Dopv::Errors::ProviderError, "No such template #{@config[:image]}" unless id
+          raise Errors::ProviderError, "No such template #{@config[:image]}" unless id
           id
         end
       end
