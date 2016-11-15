99c1d30f7755a2215b07f5116b5c2ee9e50b8b4a
Pavol Dilung <pavol.dilung@swisscom.com>
2016-11-15 21:20:16 +0100

    [log] Update logging

--- lib/dopv/log.rb
+++ lib/dopv/log.rb
@@ -11,26 +11,31 @@ module Dopv
     :info     => :green,
     :debug    => :default
   }
+  
+  def self.logger=(logger)
+    @log = logger
+    ::DopCommon.logger = logger
+  end
 
-  def self.log_init(logfile)
-    @@log ||= Logger.new(logfile)
-    @@log.formatter = proc do |severity, datetime, progname, msg|
+  def self.init_file_logger(logfile = STDOUT)
+    ::Dopv.logger = Logger.new(logfile)
+    @log.formatter = file_logger_formatter
+    @log.level = Logger::INFO
+    ::DopCommon.add_log_junction(@log)
+  end
+
+  def self.file_logger_formatter
+    Proc.new do |severity, datetime, progname, msg|
       timestamp = datetime.strftime("%Y-%m-%d %H:%M:%S.%L")
       hostname = ::Socket.gethostname.split('.').first
       file, line, method = caller[4].sub(/.*\/([0-9a-zA-Z_\-.]+):(\d+):.+`(.+)'/, "\\1-\\2-\\3").gsub(":", " ").split("-")
       pid = "[#{::Process.pid}]".ljust(7)
       logentry  = "#{timestamp} #{hostname} #{progname}#{pid} #{severity.ljust(5)} #{file}:#{line}:#{method}: #{msg}\n"
-      logfile == STDOUT ? logentry.colorize(LOG_COLORS[severity.downcase.to_sym]) : logentry
+      logentry.colorize(LOG_COLORS[severity.downcase.to_sym])
     end
-    @@log.level = Logger::INFO
-    @@log
   end
 
-  def self.log(logfile = STDOUT)
-    @@log ||= log_init(logfile)
-  end
-
-  def self.logger=(logger)
-    @@log = logger
+  def self.log
+    @log ||= (Dopv.logger = Logger.new(STDOUT))
   end
 end
