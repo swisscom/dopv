adfc35cff4b94e037613e05abffd81e9ab602699
Pavol Dilung <pavol.dilung@swisscom.com>
2016-12-19 17:24:01 +0100

    do not log color escapes into file

--- lib/dopv/log.rb
+++ lib/dopv/log.rb
@@ -19,18 +19,19 @@ module Dopv
 
   def self.init_file_logger(logfile = STDOUT)
     ::Dopv.logger = Logger.new(logfile)
-    @log.formatter = file_logger_formatter
+    @log.formatter = file_logger_formatter(logfile)
     @log.level = Logger::INFO
   end
 
-  def self.file_logger_formatter
+  def self.file_logger_formatter(logfile)
     Proc.new do |severity, datetime, progname, msg|
       timestamp = datetime.strftime("%Y-%m-%d %H:%M:%S.%L")
       hostname = ::Socket.gethostname.split('.').first
       file, line, method = caller[4].sub(/.*\/([0-9a-zA-Z_\-.]+):(\d+):.+`(.+)'/, "\\1-\\2-\\3").gsub(":", " ").split("-")
       pid = "[#{::Process.pid}]".ljust(7)
       logentry  = "#{timestamp} #{hostname} #{progname}#{pid} #{severity.ljust(5)} #{file}:#{line}:#{method}: #{msg}\n"
-      logentry.colorize(LOG_COLORS[severity.downcase.to_sym])
+      logfile == STDOUT ?
+        logentry.colorize(LOG_COLORS[severity.downcase.to_sym]) : logentry
     end
   end
 
