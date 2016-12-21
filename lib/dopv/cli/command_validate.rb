c2b4d9ffa92c4cf56bef4304aef2c6a254a935e9
Pavol Dilung <pavol.dilung@swisscom.com>
2016-12-21 16:28:38 +0100

    deprecate required options

--- lib/dopv/cli/command_validate.rb
+++ lib/dopv/cli/command_validate.rb
@@ -4,20 +4,38 @@ module Dopv
     def self.command_validate(base)
       base.class_eval do
 
-        desc 'Validate plan file'
+        desc 'Validate a plan file'
+        arg_name 'plan_file'
         command :validate do |c|
-          c.flag [:plan, :p], :arg_name => 'path_to_plan_file', :required => true
-          c.action do |global_options,options,args|
-            if Dopv.valid?(options[:plan])
+          c.flag [:plan, :p], :arg_name => 'plan_file'
+          c.action do |global_options, options, args|
+            if args.empty?
+              if options[:plan].nil?
+                exit_now!('The -p/--plan option is required.')
+              else
+                Dopv.log.warn('This invocation method is deprecated and will be removed in DOPv >= 0.8.0.')
+                Dopv.log.warn('Please use dopv validate <plan_file>.')
+                plan_file = options[:plan]
+              end
+            else
+              if args.length != 1
+                exit_now!('Validate takes excatly one argument, a plan file')
+              else
+                plan_file = args[0]
+              end
+            end
+
+            exit_now!("The #{plan_file} must exist and be a readable file") unless
+              File.file?(plan_file) && File.readable?(plan_file)
+
+            if Dopv.valid?(plan_file)
               puts('Plan valid.')
             else
               exit_now!('Plan is NOT valid!')
             end
           end
         end
-
       end
     end
-
   end
 end
